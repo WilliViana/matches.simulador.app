f2f9886737f7ce49d0cbf7bdd76ffd95b605b582
Willi Viana <willi.villi.viana@hotmail.com>
2022-05-26 21:13:05 -0300

    Desenvolvimento dos layouts de listagem e detalhes de partidas.

---
+++ app/src/main/java/me/dio/simulator/DetailActivity2.kt
@@ -0,0 +1,20 @@
+package me.dio.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.dio.simulator.databinding.ActivityDetail2Binding
+
+class DetailActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetail2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetail2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
